681cb365cf59a026f0f7dc0b41654ccdf5b3ae81
Thomas Kluyver <takowl@gmail.com>
2011-06-15 13:55:33 +0100

    Add copyright information.

--- conf.py
+++ conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u''
-copyright = u''
+project = u'IPython'
+copyright = u'the IPython development team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
