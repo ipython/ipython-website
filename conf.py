5d50efee4c3ff6a34c73394471aabd372e7445aa
Francisco de la Peña <frandelapena@gmail.com>
2011-08-19 23:17:06 +0100

    Corrected typo

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Hyperspy'
-copyright = u'The Hyperespy development team'
+copyright = u'The Hyperspy development team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
